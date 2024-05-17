aa360a8a292edc83b8d9da3d7b78d5640590dee7
FarrelBeso <vabeso@up.edu.ph>
2024-05-17 16:16:53 +0800

    test: tested the get reviews from establishments function

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import getEstablishments from "./commands/establishment_cmds.js";
+import getReviewsFromEstablishments from "./commands/review_cmds.js";
 import { connectDB, disconnectDB } from "./db/connectDB.js";
 
 // try to connect and disconnect
 const conn = await connectDB();
 // try to get establishments
 await getEstablishments(conn);
+await getReviewsFromEstablishments(conn, { id: 1 });
 await disconnectDB(conn);
 
 // finally exit
