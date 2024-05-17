a3ed39b2ffbed0e98a7a42655b70f4bd1a45434c
FarrelBeso <vabeso@up.edu.ph>
2024-05-17 15:18:46 +0800

    feat: added the folders and connectdb file

---
+++ db/connectDB.js
@@ -0,0 +1,35 @@
+import mariadb from "mariadb";
+
+const pool = mariadb.createPool({
+  host: "localhost",
+  user: "foodreviewer",
+  password: "foodreview123",
+  database: "food_reviewer",
+  connectionLimit: 5,
+});
+
+export async function connectDB() {
+  let conn;
+  try {
+    const spinner = ora("Connecting to the database...").start();
+    conn = await pool.getConnection();
+    spinner.stop();
+    console.log(chalk.greenBright("Successfully connected to database."));
+    return conn;
+  } catch (error) {
+    console.log(chalk.redBright("Error: "), error);
+    process.exit(1);
+  }
+}
+
+export async function disconnectDB(conn) {
+  try {
+    const spinner = ora("Disconnecting from the database...").start();
+    conn.release();
+    spinner.stop();
+    console.log(chalk.greenBright("Successfully connected to database."));
+  } catch (error) {
+    console.log(chalk.redBright("Error: "), error);
+    process.exit(1);
+  }
+}
