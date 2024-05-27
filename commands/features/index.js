a2a7c7e4983003e48a60a9d44a1ad9e570fd9a4c
Alemph <jlsangregorio@up.edu.ph>
2024-05-28 04:41:50 +0800

    fixed index.js for features

--- commands/features/index.js
+++ commands/features/index.js
@@ -1,5 +1,10 @@
 import feature_1 from "./feature_1/index.js";
+import feature_2 from "./feature_2/index.js";
+import feature_3 from "./feature_3/index.js";
 
 export default {
-  ...feature_1,
+  feature_1,
+  feature_2,
+  feature_3,
 };
+
