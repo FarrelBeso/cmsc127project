cd475c58bf42a7ff9de8ab75082de072974228a6
Paulene Cagna-an <100839836+paucagnaan@users.noreply.github.com>
2024-06-02 13:35:42 +0800

    Create getItemsPriceOrder.js

---
+++ commands/reports/getItemsPriceOrder.js
@@ -0,0 +1,54 @@
+import chalk from "chalk";
+import ora from "ora";
+import inquirer from "inquirer";
+import { connectDB, disconnectDB } from "../../db/connectDB.js";
+
+/**
+ * View the food items from an establishment arranged according to price.
+ */
+export async function getItemsPriceOrder() {
+  let conn;
+  try {
+    // connect to db
+    conn = await connectDB() {
+      // Ask user whether to arrange price list in increasing or decreasing order
+      const validSortOrder = sort === "DESC" ? "DESC" : "ASC";
+      // check
+      const foodItem = await inquirer.prompt([
+        {
+          name: "id",
+          message: "Enter the id of the establishment:",
+          type: "input",
+          validate: function(value) {
+            var valid = !isNaN(parseFloat(value));
+            return valid || "Please enter a number.";
+          },
+          filter: Number
+        },
+        {
+          name: "sortOrder",
+          message: "How would you like to arrange the food items by price?",
+          type: "list",
+          choices: ["ASC","DESC"]
+        },
+      ]);
+
+      // starting the spinner
+      const spinner = ora("Sorting and displaying food items...").start();
+      const foodItem = await conn.query("SELECT * FROM food_item WHERE establishment_id=? ORDER BY price $(validSortOrder)",
+                                        [foodItem.id, foodItem.sortOrder,]
+      );
+      // stop the spinner
+      spinner.stop();
+                
+      await disconnectDB(conn);
+      } catch (error) {
+        // Error Handling
+        console.log(chalk.redBright(`Something went wrong, Error: ${error}`));
+        if (conn) await disconnectDB(conn);
+        process.exit(1);
+      }
+
+      // close the program
+      process.exit(0);
+    }
