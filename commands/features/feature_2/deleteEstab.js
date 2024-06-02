5f207d05e26f65c6ab9baf3762152699cb38f168
FarrelBeso <vabeso@up.edu.ph>
2024-06-02 22:14:19 +0800

    chore: attempt to delete estab function

--- commands/features/feature_2/deleteEstab.js
+++ commands/features/feature_2/deleteEstab.js
@@ -1,6 +1,7 @@
 import chalk from "chalk";
 import ora from "ora";
 import inquirer from "inquirer";
+import CliTable3 from "cli-table3";
 import { connectDB, disconnectDB } from "../../../db/connectDB.js";
 import { login } from "../../additional_features/auth_cmds.js";
 
@@ -8,15 +9,50 @@ import { login } from "../../additional_features/auth_cmds.js";
  * Delete a food establishment.
  */
 export async function deleteEstab() {
-  let conn;
+  let conn, spinner, table;
   try {
     // connect to db
     conn = await connectDB();
     // first try to login
     const loginResponse = await login(conn);
-    if (!loginResponse.success || loginResponse.user.usertype !== 'admin') {
+    if (!loginResponse.success || loginResponse.user.usertype !== "admin") {
       throw "Only admin users can delete a food establishment.";
     }
+
+    // show all establishments first
+    // starting the spinner
+    spinner = ora("Searching establishments...").start();
+    // searching in the database
+    const establishments = await conn.query(
+      "SELECT * FROM food_establishment ORDER BY name"
+    );
+    // stopping the spinner
+    spinner.stop();
+
+    if (establishments.length === 0) {
+      console.log(chalk.blueBright("No establishments found."));
+      process.exit(0);
+    }
+
+    // show table here
+    table = new CliTable3({
+      head: [
+        chalk.green("Establishment ID"),
+        chalk.green("Name"),
+        chalk.green("Address"),
+        chalk.green("Email"),
+      ],
+    });
+    // loop for all items
+    for (let tuple of establishments) {
+      table.push([
+        tuple.establishment_id,
+        tuple.name,
+        tuple.address,
+        tuple.email,
+      ]);
+    }
+    console.log(table.toString());
 
     // query the user for establishment ID
     const answers = await inquirer.prompt([
@@ -27,14 +63,20 @@ export async function deleteEstab() {
       },
     ]);
 
-    // starting the spinner
-    const spinner = ora("Deleting establishment...").start();
-    // deleting from the database
+    // deleting related reviews
+    spinner = ora("Dereferencing related reviews...").start();
+    await conn.query(
+      "UPDATE FROM review SET establishment_id=NULL WHERE establishment_id = ?",
+      [answers.id]
+    );
+    spinner.stop();
+
+    // delete the establishment itself
+    spinner = ora("Deleting establishment...").start();
     await conn.query(
       "DELETE FROM food_establishment WHERE establishment_id = ?",
       [answers.id]
     );
-    // stopping the spinner
     spinner.stop();
 
     console.log(chalk.greenBright("Food establishment deleted successfully!"));
