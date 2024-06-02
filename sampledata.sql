fb0fc1fdd26ae2362efbcb2548f77513cc5e457c
FarrelBeso <vabeso@up.edu.ph>
2024-06-02 22:30:54 +0800

    fix: data fix in sample data.sql

--- sampledata.sql
+++ sampledata.sql
@@ -1,6 +1,6 @@
 -- Insert users
 INSERT INTO user (first_name, last_name, usertype, email, hashed_password) VALUES("admin", "admin", "admin", "admin@admin.com", "$2b$10$UuXc9wmOe061Lj9K5u.y4.x9C8SUJxKv4XlOh6m6m2Q3OyBi5/YsW");
-INSERT INTO user (first_name, last_name, usertype, email, hashed_password) VALUES("test", "test", "test", "test@test.com", "$2b$10$qDfbCGpZextrDflUXj3YDujIsd91kz.bHUGWBKTzGrXiqDOJ5n/iC");
+INSERT INTO user (first_name, last_name, usertype, email, hashed_password) VALUES("test", "test", "user", "test@test.com", "$2b$10$qDfbCGpZextrDflUXj3YDujIsd91kz.bHUGWBKTzGrXiqDOJ5n/iC");
 
 -- Insert food establishments
 INSERT INTO food_establishment (name, address, email) VALUES("Jollibee", "123 Apple St., Baker City", "jolibee@email.com");
@@ -23,7 +23,7 @@ INSERT INTO review (rating, user_id, description, establishment_id) VALUES (2, 2
 INSERT INTO review (rating, user_id, description, establishment_id) VALUES (4, 2, "yummy", 2);
 INSERT INTO review (rating, user_id, description, establishment_id) VALUES (4, 2, "yummy", 3);
 -- Dummy Review for testing
-INSERT INTO review (review_date, rating, user_id, description, establishment_id) VALUES ('2020-01-01 00:00:00', 4, 3, "yummy", 3);
+INSERT INTO review (review_date, rating, user_id, description, establishment_id) VALUES ('2020-01-01 00:00:00', 4, 2, "yummy", 3);
 
 -- Food Reviews
 INSERT INTO review (rating, user_id, description, food_id) VALUES (4, 2, "yummy", 1);
@@ -32,7 +32,7 @@ INSERT INTO review (rating, user_id, description, food_id) VALUES (2, 2, "yummy"
 INSERT INTO review (rating, user_id, description, food_id) VALUES (4, 2, "yummy", 2);
 INSERT INTO review (rating, user_id, description, food_id) VALUES (4, 2, "yummy", 3);
 -- Dummy Review for testing
-INSERT INTO review (review_date, rating, user_id, description, food_id) VALUES ('2020-01-01 00:00:00', 4, 3, "yummy", 3);
+INSERT INTO review (review_date, rating, user_id, description, food_id) VALUES ('2020-01-01 00:00:00', 4, 2, "yummy", 3);
 
 -- Insert Item Type
 INSERT INTO food_item_type (food_id, type) VALUES (1, "meat");
