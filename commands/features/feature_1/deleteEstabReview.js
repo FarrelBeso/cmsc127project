c87919e5a71c025d2fedde1e2b05a9056855f314
FarrelBeso <vabeso@up.edu.ph>
2024-06-02 17:07:46 +0800

    chore: delete estab review

--- commands/features/feature_1/deleteEstabReview.js
+++ commands/features/feature_1/deleteEstabReview.js
@@ -5,10 +5,10 @@ import { connectDB, disconnectDB } from "../../../db/connectDB.js";
 import { login } from "../../additional_features/auth_cmds.js";
 
 /**
- * Delete a review for an establishment.
+ * Update a review for an establishment.
  */
 export async function deleteEstabReview() {
-  let conn;
+  let conn, spinner;
   try {
     // connect to db
     conn = await connectDB();
@@ -18,30 +18,79 @@ export async function deleteEstabReview() {
       throw loginResponse.msg;
     }
 
+    spinner = ora("Fetching your reviews...").start();
+
+    // select which reviews has been made by the user
+    const userReviews = await conn.query(
+      "SELECT * FROM review WHERE user_id=? AND establishment_id IS NOT NULL",
+      [loginResponse.user.user_id]
+    );
+
+    spinner.stop();
+
+    if (userReviews.length === 0) {
+      console.log(
+        chalk.blueBright("You have no reviews made yet. Try adding one!")
+      );
+      process.exit(0);
+    }
+
+    console.log(userReviews);
+
     // query the user if the establishment exists
-    const checkPrompt = await inquirer.prompt([
+    const establishmentIdPrompt = await inquirer.prompt([
       {
         name: "id",
         message: "Enter the id of the establishment:",
         type: "input",
       },
     ]);
-    const establishments = await conn.query(
-      "SELECT * FROM food_establishment WHERE establishment_id=?",
-      [checkPrompt.id]
+
+    spinner = ora("Fetching your reviews...").start();
+
+    const establishmentReviews = await conn.query(
+      "SELECT * FROM review WHERE user_id=? AND establishment_id=?",
+      [loginResponse.user.user_id, establishmentIdPrompt.id]
     );
 
-    if (establishments.length === 0) {
-      throw "Establishment does not exist.";
+    spinner.stop();
+
+    if (establishmentReviews.length === 0) {
+      console.log(chalk.blueBright("Establishment does not exist."));
+      process.exit(0);
+    }
+
+    console.log(establishmentReviews);
+
+    // then select which review id to select
+    const reviewIdPrompt = await inquirer.prompt([
+      {
+        name: "id",
+        message: "Select the id of the review:",
+        type: "input",
+      },
+    ]);
+
+    spinner = ora("Fetching review...").start();
+
+    const review = await conn.query(
+      "SELECT * FROM review WHERE review_id=? AND user_id=? AND establishment_id=?",
+      [reviewIdPrompt.id, loginResponse.user.user_id, establishmentIdPrompt.id]
+    );
+
+    spinner.stop();
+
+    if (review.length === 0) {
+      console.log(chalk.blueBright("Review does not exist."));
+      process.exit(0);
     }
 
     // starting the spinner
-    const spinner = ora("Deleting review...").start();
-    // deleting the review in the database
-    await conn.query(
-      "DELETE FROM review WHERE establishment_id=? AND user_id=?",
-      [checkPrompt.id, loginResponse.user.user_id]
-    );
+    spinner = ora("Deleting review...").start();
+    // updating the review in the database
+    await conn.query("DELETE FROM review WHERE review_id = ?", [
+      review[0].review_id,
+    ]);
     // stopping the spinner
     spinner.stop();
 
