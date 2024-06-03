69d58c1874716ee26a3da2a61e14e1bd16981917
FarrelBeso <vabeso@up.edu.ph>
2024-06-03 12:34:55 +0800

    chore: added table in get all items

--- commands/additional_features/misc/getAllItems.js
+++ commands/additional_features/misc/getAllItems.js
@@ -1,26 +1,52 @@
 import chalk from "chalk";
 import ora from "ora";
+import CliTable3 from "cli-table3";
 import { connectDB, disconnectDB } from "../../../db/connectDB.js";
 
 /**
  * View all food items.
  */
 export async function getAllItems() {
-  let conn;
+  let conn, spinner, table;
   try {
     // connect first
     conn = await connectDB();
-    // starting the spinner
-    const spinner = ora("Fetching all items...").start();
-    const items = await conn.query("SELECT * FROM food_item");
-    // stopping the spinner
+
+    // get all items
+    spinner = ora("Fetching all food items...").start();
+    const items = await conn.query(
+      "SELECT f.food_id, f.name, f.price, f.availability, e.name establishment_name FROM food_item f \
+    JOIN food_establishment e ON f.establishment_id=e.establishment_id \
+    ORDER BY e.name, f.name"
+    );
     spinner.stop();
 
     if (items.length === 0) {
-      console.log(chalk.blueBright("Items not found."));
-    } else {
-      console.log(items);
+      console.log(chalk.blueBright("Food items not found."));
+      process.exit(0);
     }
+
+    // otherwise, show the item table
+    table = new CliTable3({
+      head: [
+        chalk.green("Food ID"),
+        chalk.green("Name"),
+        chalk.green("Price"),
+        chalk.green("Availability"),
+        chalk.green("Establishment Name"),
+      ],
+    });
+    for (let tuple of items) {
+      table.push([
+        tuple.food_id,
+        tuple.name,
+        tuple.price,
+        tuple.availability === 1 ? "Available" : "Not Available",
+        tuple.establishment_name,
+      ]);
+    }
+    console.log(table.toString());
+
     // finally disconnect
     await disconnectDB(conn);
   } catch (error) {
