25c5152662fd80e91e2411a81db845227d0547fd
FarrelBeso <vabeso@up.edu.ph>
2024-06-03 08:38:11 +0800

    feat: init adding of update user

--- commands/additional_features/users/updateUser.js
+++ commands/additional_features/users/updateUser.js
@@ -3,37 +3,28 @@ import ora from "ora";
 import inquirer from "inquirer";
 import CliTable3 from "cli-table3";
 import { connectDB, disconnectDB } from "../../../db/connectDB.js";
+import { login } from "../../additional_features/auth_cmds.js";
 
 /**
- * Search for a user.
+ * Update a user.
  */
 export async function updateUser() {
-  let conn, table;
+  let conn, spinner, table;
   try {
-    // search query
+    // login first
     conn = await connectDB();
-    const answers = await inquirer.prompt([
-      {
-        name: "searchTerm",
-        message: "Search for the first name / last name / email of the user:",
-        type: "input",
-      },
-    ]);
+    const loginResponse = await login(conn);
+    if (!loginResponse.success || loginResponse.user.usertype !== "admin") {
+      throw "Only admin users can update users.";
+    }
 
-    // searching in the database
-    const spinner = ora("Searching users...").start();
-    const results = await conn.query(
-      "SELECT * FROM user WHERE first_name LIKE ? OR last_name LIKE ? OR email LIKE ?",
-      [
-        `%${answers.searchTerm.toLowerCase()}%`,
-        `%${answers.searchTerm.toLowerCase()}%`,
-        `%${answers.searchTerm.toLowerCase()}%`,
-      ]
-    );
+    // show all users first
+    spinner = ora("Searching users...").start();
+    const users = await conn.query("SELECT * FROM user ORDER BY last_name");
     spinner.stop();
 
-    // exit early if there are none found
-    if (results.length === 0) {
+    // exit early if none found
+    if (users.length === 0) {
       console.log(chalk.blueBright("No users found."));
       process.exit(0);
     }
@@ -48,7 +39,7 @@ export async function updateUser() {
         chalk.green("Email"),
       ],
     });
-    for (let tuple of results) {
+    for (let tuple of users) {
       table.push([
         tuple.user_id,
         tuple.first_name,
@@ -59,7 +50,57 @@ export async function updateUser() {
     }
     console.log(table.toString());
 
-    // finish
+    // query the user for user ID
+    const userIdPrompt = await inquirer.prompt([
+      {
+        name: "id",
+        message: "Enter the id of the user to update:",
+        type: "input",
+      },
+    ]);
+
+    // get that user
+    spinner = ora("Fetching user...").start();
+    const user = await conn.query("SELECT * FROM user where user_id=?", [
+      userIdPrompt.id,
+    ]);
+    spinner.stop();
+
+    // end program if that user doesn't exist
+    if (user.length === 0) {
+      console.log(chalk.blueBright("User does not exist."));
+      process.exit(0);
+    }
+
+    // update data
+    const answers = await inquirer.prompt([
+      {
+        name: "first_name",
+        message: "Enter the new first name of the user:",
+        type: "input",
+      },
+      {
+        name: "last_name",
+        message: "Enter the new last name of the user:",
+        type: "input",
+      },
+      {
+        name: "email",
+        message: "Enter the new email of the user:",
+        type: "input",
+      },
+    ]);
+
+    // update the document
+    spinner = ora("Updating user...").start();
+    await conn.query(
+      "UPDATE user SET first_name = ?, last_name = ?, email = ? WHERE user_id = ?",
+      [answers.first_name, answers.last_name, answers.email, userIdPrompt.id]
+    );
+    spinner.stop();
+
+    // operation confirm
+    console.log(chalk.greenBright("User updated successfully!"));
     await disconnectDB(conn);
   } catch (error) {
     // Error Handling
