ed6d156dfa6812ac13686534d8a7c411b3f91e8b
FarrelBeso <vabeso@up.edu.ph>
2024-06-03 13:12:42 +0800

    chore: get all estabs show table and also get reviews from estabs

--- commands/reports/getReviewsFromEstabs.js
+++ commands/reports/getReviewsFromEstabs.js
@@ -1,18 +1,52 @@
 import chalk from "chalk";
 import ora from "ora";
 import inquirer from "inquirer";
+import CliTable3 from "cli-table3";
 import { connectDB, disconnectDB } from "../../db/connectDB.js";
 
 /**
  * View all food reviews from an establishment.
  */
 export async function getReviewsFromEstabs() {
-  let conn;
+  let conn, spinner, table;
   try {
     // connect to db
     conn = await connectDB();
+
+    // show all establishments first
+    spinner = ora("Searching establishments...").start();
+    const establishments = await conn.query(
+      "SELECT * FROM food_establishment ORDER BY name"
+    );
+    spinner.stop();
+
+    // exit if there are none
+    if (establishments.length === 0) {
+      console.log(chalk.blueBright("No establishments found."));
+      process.exit(0);
+    }
+
+    // show table here
+    table = new CliTable3({
+      head: [
+        chalk.green("Establishment ID"),
+        chalk.green("Name"),
+        chalk.green("Address"),
+        chalk.green("Email"),
+      ],
+    });
+    for (let tuple of establishments) {
+      table.push([
+        tuple.establishment_id,
+        tuple.name,
+        tuple.address,
+        tuple.email,
+      ]);
+    }
+    console.log(table.toString());
+
     // first query the user on the id
-    const answers = await inquirer.prompt([
+    const establishmentIdPrompt = await inquirer.prompt([
       {
         name: "id",
         message: "Enter the id of the establishment:",
@@ -21,21 +55,40 @@ export async function getReviewsFromEstabs() {
     ]);
 
     // starting the spinner
-    const spinner = ora("Fetching reviews from establishments...").start();
+    spinner = ora("Fetching reviews from establishments...").start();
     // fetching all the establishments from the database
     const reviews = await conn.query(
-      "SELECT * FROM review where establishment_id=?",
-      [answers.id]
+      "SELECT * FROM review where establishment_id=? ORDER BY review_date DESC",
+      [establishmentIdPrompt.id]
     );
     // stopping the spinner
     spinner.stop();
 
-    // check if establishments exist or not
-    if (reviews.length === 0) {
-      console.log(chalk.blueBright("Reviews not found."));
-    } else {
-      console.log(reviews);
+    // exit if there are none
+    if (establishments.length === 0) {
+      console.log(chalk.blueBright("No establishments found."));
+      process.exit(0);
     }
+
+    // show table
+    table = new CliTable3({
+      head: [
+        chalk.green("Review ID"),
+        chalk.green("Review Date"),
+        chalk.green("Rating"),
+        chalk.green("Description"),
+      ],
+    });
+    for (let tuple of reviews) {
+      table.push([
+        tuple.review_id,
+        format(tuple.review_date.toString(), "yyyy-MM-dd HH:mm:ss"),
+        tuple.rating,
+        tuple.description,
+      ]);
+    }
+    console.log(table.toString());
+
     await disconnectDB(conn);
   } catch (error) {
     // Error Handling
