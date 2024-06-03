1bcb793f1ffa10c047b32fe84e3f8ff3752cb5ad
FarrelBeso <vabeso@up.edu.ph>
2024-06-03 08:44:30 +0800

    feat: delete user

--- commands/additional_features/users/deleteUser.js
+++ commands/additional_features/users/deleteUser.js
@@ -3,37 +3,28 @@ import ora from "ora";
 import inquirer from "inquirer";
 import CliTable3 from "cli-table3";
 import { connectDB, disconnectDB } from "../../../db/connectDB.js";
+import { login } from "../../additional_features/auth_cmds.js";
 
 /**
- * Search for a user.
+ * Delete a user.
  */
 export async function deleteUser() {
-  let conn, table;
+  let conn, spinner, table;
   try {
-    // search query
+    // login first
     conn = await connectDB();
-    const answers = await inquirer.prompt([
-      {
-        name: "searchTerm",
-        message: "Search for the first name / last name / email of the user:",
-        type: "input",
-      },
-    ]);
+    const loginResponse = await login(conn);
+    if (!loginResponse.success || loginResponse.user.usertype !== "admin") {
+      throw "Only admin users can update users.";
+    }
 
-    // searching in the database
-    const spinner = ora("Searching users...").start();
-    const results = await conn.query(
-      "SELECT * FROM user WHERE first_name LIKE ? OR last_name LIKE ? OR email LIKE ?",
-      [
-        `%${answers.searchTerm.toLowerCase()}%`,
-        `%${answers.searchTerm.toLowerCase()}%`,
-        `%${answers.searchTerm.toLowerCase()}%`,
-      ]
-    );
+    // show all users first
+    spinner = ora("Searching users...").start();
+    const users = await conn.query("SELECT * FROM user ORDER BY last_name");
     spinner.stop();
 
-    // exit early if there are none found
-    if (results.length === 0) {
+    // exit early if none found
+    if (users.length === 0) {
       console.log(chalk.blueBright("No users found."));
       process.exit(0);
     }
@@ -48,7 +39,7 @@ export async function deleteUser() {
         chalk.green("Email"),
       ],
     });
-    for (let tuple of results) {
+    for (let tuple of users) {
       table.push([
         tuple.user_id,
         tuple.first_name,
@@ -59,7 +50,22 @@ export async function deleteUser() {
     }
     console.log(table.toString());
 
-    // finish
+    // query the user for user ID
+    const userIdPrompt = await inquirer.prompt([
+      {
+        name: "id",
+        message: "Enter the id of the user to delete:",
+        type: "input",
+      },
+    ]);
+
+    // deleting the document
+    spinner = ora("Deleting user...").start();
+    await conn.query("DELETE FROM user WHERE user_id = ?", [userIdPrompt.id]);
+    spinner.stop();
+
+    // operation confirm
+    console.log(chalk.greenBright("User deleted successfully!"));
     await disconnectDB(conn);
   } catch (error) {
     // Error Handling
